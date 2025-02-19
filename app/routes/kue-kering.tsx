2e561945b4fc598e2564b706e94a203f699b9fc3
Zidane Putra Fadilah <hackiyo@ail.com>
2025-02-19 13:07:44 +0700

    buat galeri

--- app/routes/kue-kering.tsx
+++ app/routes/kue-kering.tsx
@@ -1,13 +1,25 @@
+import "./galeri.css";
+
 export function meta() {
-    return [{ title: "Aneka Kue Kering - Nizie's Cake" }];
-  }
-  
-  export default function KueKering() {
-    return (
-      <div>
-        <h1>Aneka Kue Kering</h1>
-        <p>Selamat datang di halaman Aneka Kue Kering! 🎂</p>
+  return [{ title: "Aneka Kue Kering - Nizie's Cake" }];
+}
+
+const kueList = [
+  { name: "Nastar", image: "/app/images/kuker/nastar.jpg" },
+];
+
+export default function KueKering() {
+  return (
+    <div className="galeri-container">
+      <h1 className="galeri-title">Aneka Kue Kering</h1>
+      <div className="grid-container">
+        {kueList.map((kue, index) => (
+          <div key={index} className="card">
+            <img src={kue.image} alt={kue.name} className="kue-image" />
+            <div className="kue-name">{kue.name}</div>
+          </div>
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
